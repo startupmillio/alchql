cb2323d599df7ce9cad6651906304c1addde97f3
Dmitry Karnei <5783354@gmail.com>
2021-10-26 16:09:45 +0200

    fix camel case issue

--- graphene_sqlalchemy_core/gql_fields.py
+++ graphene_sqlalchemy_core/gql_fields.py
@@ -28,8 +28,10 @@ from graphql import FieldNode, FragmentDefinitionNode, GraphQLResolveInfo
 
 
 def camel_to_snake(name: str) -> str:
-    name = re.sub("(.)([0-9]+[A-Z]*[a-z]+)", r"\1_\2", name)
-    return re.sub("([a-z0-9_])([A-Z])", r"\1_\2", name).lower()
+    name = re.sub(
+        r"((?!^[a-z0-9]*)|\b[a-zA-Z][a-z0-9]*)([A-Z][a-z]*|\d+)", r"\1_\2", name
+    )
+    return name.lower()
 
 
 def collect_fields(node, fragments):
