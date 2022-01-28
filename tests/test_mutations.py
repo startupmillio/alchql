03c2f53c57db8a29774e7678ae82abf44e1fe927
Myrik <myrik260138@tut.by>
2022-01-28 16:51:45 +0100

    fix tests

--- tests/test_mutations.py
+++ tests/test_mutations.py
@@ -12,69 +12,6 @@ from .models import Pet
 from .test_query import add_test_data
 
 
-async def _run_cases(session, Query, Mutation):
-    query = """
-        query {
-            allPets {
-                edges {
-                    node {
-                        id
-                        name
-                    }
-                }
-            }
-        }
-    """
-
-    schema = graphene.Schema(
-        query=Query,
-        mutation=Mutation,
-    )
-    result = await schema.execute_async(
-        query,
-        context_value=Context(session=session),
-        middleware=[
-            LoaderMiddleware([Pet]),
-        ],
-    )
-    assert not result.errors
-
-    all_pets = [i["node"] for i in result.data["allPets"]["edges"]]
-
-    assert [i["name"] for i in all_pets] == [
-        "Garfield",
-        "Lassie",
-    ]
-
-    query = """
-        mutation UpdatePet($value: InputPet!, $updatePetId: ID!) {
-            updatePet(value: $value, id: $updatePetId) {
-                id
-                name
-            }
-        }
-    """
-
-    id_to_update = all_pets[0]["id"]
-    new_name = "pedobear"
-
-    result = await schema.execute_async(
-        query,
-        variables={
-            "value": {"name": new_name},
-            "updatePetId": id_to_update,
-        },
-        context_value=Context(session=session),
-        middleware=[
-            LoaderMiddleware([Pet]),
-        ],
-    )
-
-    assert not result.errors
-    assert result.data["updatePet"]["id"] == id_to_update
-    assert result.data["updatePet"]["name"] == new_name
-
-
 @pytest.mark.asyncio
 async def test_get_update_mutation_case1(session):
     await add_test_data(session)
@@ -305,13 +242,13 @@ async def test_get_update_mutation_case3(session):
     ]
 
     query = """
-            mutation UpdatePet($value: InputPet!, $updatePetId: ID!) {
-                updatePet(value: $value, id: $updatePetId) {
-                    id
-                    name
-                }
+        mutation UpdatePet($value: InputPet!, $updatePetId: ID!) {
+            updatePet(value: $value, id: $updatePetId) {
+                id
+                name
             }
-        """
+        }
+    """
 
     id_to_update = all_pets[0]["id"]
     new_name = "pedobear"
@@ -400,7 +337,7 @@ async def test_get_update_mutation_empty_value(session):
     id_to_update = all_pets[0]["id"]
 
     with pytest.raises(Exception) as e:
-        await schema.execute_async(
+        result = await schema.execute_async(
             query,
             variables={
                 "value": {},
@@ -411,5 +348,5 @@ async def test_get_update_mutation_empty_value(session):
                 LoaderMiddleware([Pet]),
             ],
         )
-
-    assert str(e.value) == "No value provided"
+        assert result.errors
+        raise result.errors[0]
