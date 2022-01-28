28fca8972f0ab3e95e920a0973df0711fe16784f
Myrik <myrik260138@tut.by>
2022-01-28 14:18:48 +0100

    update deps

--- setup.py
+++ setup.py
@@ -25,9 +25,8 @@ tests_require = [
     "coverage[toml]",
     "sqlalchemy_utils>=0.37.0,<1.0",
     "pytest-benchmark>=3.4.0,<4.0",
-    "mock == 4.0.3",
-    "pytest-asyncio == 0.15.1",
-    "aiosqlite == 0.17.0",
+    "pytest-asyncio>=0.17.2,<0.18",
+    "aiosqlite>=0.17.0,<0.18",
 ]
 
 setup(
